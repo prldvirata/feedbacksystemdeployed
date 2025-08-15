707ef908c4bf0c4ecad9990da952ac3af69afcdd
Patrice Remy Lou D Virata <trish.virata@gmail.com>
2025-08-15 10:04:54 -0500

    Update settings.py

--- feedbacksystem/settings.py
+++ feedbacksystem/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
